c166b9c03570925adc7e5113f78ae7748f2ade58
Mikhail-Kranyshev <91139884+Mikhail-Kranyshev@users.noreply.github.com>
2022-07-22 12:20:22 +0700

    Stage 3/6: Estimate the number of servings

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,20 +1,34 @@
 package machine;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
     public static void main(String[] args) {
+        final String[] msgs = {"ml of water the coffee machine has:",
+                "ml of milk the coffee machine has:",
+                "grams of coffee beans the coffee machine has:",
+                "cups of coffee you will need:"};
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need:");
+        final int[] base = {200, 50, 15};
+        int[] countOfIngredient = new int[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Write how many " + msgs[i]);
+            countOfIngredient[i] = scanner.nextInt() / base[i];
+        }
+        Arrays.sort(countOfIngredient);
+        int minimumCountOfCups = countOfIngredient[0];
+
+        System.out.println("Write how many " + msgs[3]);
         int countOfCups = scanner.nextInt();
-        int countOfWater = countOfCups * 200;
-        int countOfMilk = countOfCups * 50;
-        int countOfCoffeeBeans = countOfCups * 15;
-        System.out.printf("""
-                For %d cups of coffee you will need:
-                %d ml of water
-                %d ml of milk
-                %d g of coffee beans""",
-                countOfCups, countOfWater, countOfMilk, countOfCoffeeBeans);
+        if (minimumCountOfCups > countOfCups) {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n",
+                    minimumCountOfCups - countOfCups);
+        } else if (minimumCountOfCups == countOfCups) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else {
+            System.out.printf("No, I can make only %d cup(s) of coffee\n", minimumCountOfCups);
+        }
+
     }
 }
