b6297372526e21959a44cded34c14fbba1ae3354
Mikhail-Kranyshev <91139884+Mikhail-Kranyshev@users.noreply.github.com>
2022-07-22 12:42:24 +0700

    Stage 4/6: Buy, fill, take!

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -4,31 +4,84 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    public static void main(String[] args) {
-        final String[] msgs = {"ml of water the coffee machine has:",
-                "ml of milk the coffee machine has:",
-                "grams of coffee beans the coffee machine has:",
-                "cups of coffee you will need:"};
+    static final String[] msgs = {"Write how many ml of water you want to add:",
+            "Write how many ml of milk you want to add:",
+            "Write how many grams of coffee beans you want to add:",
+            "Write how many disposable cups of coffee you want to add:"};
+    static int[] coffeeMachine = {400, 540, 120, 9, 550};
+    public static void print(int[] inventory) {
+        System.out.printf("""
+                The coffee machine has:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                %d disposable cups
+                $%d of money
+                \n""", inventory[0], inventory[1], inventory[2], inventory[3], inventory[4]);
+    }
+
+    public static void menu(){
         Scanner scanner = new Scanner(System.in);
-        final int[] base = {200, 50, 15};
-        int[] countOfIngredient = new int[3];
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Write how many " + msgs[i]);
-            countOfIngredient[i] = scanner.nextInt() / base[i];
+        System.out.println("Write action (buy, fill, take):");
+        String action = scanner.next();
+        switch (action) {
+            case "buy":
+                buy();
+                break;
+            case "fill":
+                fill();
+                break;
+            case "take":
+                take();
+                break;
         }
-        Arrays.sort(countOfIngredient);
-        int minimumCountOfCups = countOfIngredient[0];
+        System.out.println();
+        print(coffeeMachine);
+    }
 
-        System.out.println("Write how many " + msgs[3]);
-        int countOfCups = scanner.nextInt();
-        if (minimumCountOfCups > countOfCups) {
-            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n",
-                    minimumCountOfCups - countOfCups);
-        } else if (minimumCountOfCups == countOfCups) {
-            System.out.println("Yes, I can make that amount of coffee");
-        } else {
-            System.out.printf("No, I can make only %d cup(s) of coffee\n", minimumCountOfCups);
+    public static void buy() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                coffeeMachine[0] -= 250;
+                coffeeMachine[2] -= 16;
+                coffeeMachine[3]--;
+                coffeeMachine[4] += 4;
+                break;
+            case 2:
+                coffeeMachine[0] -= 350;
+                coffeeMachine[1] -= 75;
+                coffeeMachine[2] -= 20;
+                coffeeMachine[3]--;
+                coffeeMachine[4] += 7;
+                break;
+            case 3:
+                coffeeMachine[0] -= 200;
+                coffeeMachine[1] -= 100;
+                coffeeMachine[2] -= 12;
+                coffeeMachine[3]--;
+                coffeeMachine[4] += 6;
+                break;
         }
+    }
 
+    public static void fill() {
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < msgs.length; i++) {
+            System.out.println(msgs[i]);
+            coffeeMachine[i] += scanner.nextInt();
+        }
+    }
+
+    public static void take() {
+        System.out.println("I gave you $" + coffeeMachine[4]);
+        coffeeMachine[4] = 0;
+    }
+
+    public static void main(String[] args) {
+        print(coffeeMachine);
+        menu();
     }
 }
